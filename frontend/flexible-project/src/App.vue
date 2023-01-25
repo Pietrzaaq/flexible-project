83e9b723615f3d19c029f430f0932e5d5ad79802
Pietrzaaq <pietrzakd53@gmail.com>
2023-01-25 02:19:28 +0100

    Finished front-end

--- frontend/flexible-project/src/App.vue
+++ frontend/flexible-project/src/App.vue
@@ -1,42 +1,21 @@
 <script setup>
-import InputPanel from './components/InputPanel.vue'
-import UsersPanel from './components/UsersPanel.vue'
+import { ref } from 'vue'
+import Header from './components/Header.vue'
+import Users from './components/Users.vue'
+
+let users = ref([])
+
+function updateUsers(usersJson) {
+  console.log(usersJson)
+  users.value = usersJson
+}
 </script>
 
 <template>
-  <header>
-      <InputPanel/>
-  </header>
-  <main>
-    <UsersPanel />
-  </main>
+  <div class="container">
+    <Header @fetch-users="updateUsers"></Header>
+    <Users :users="users" />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
